ed9021a8c99e01ca8f1d8585991d4a9ce29ae686
I A <ilie.adrian1986@gmail.com>
2024-12-12 07:23:56 +0200

    Node class finished, added basic version for the other function and calls in main.js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-import {  } from "./linkedlists.js";
+import { LinkedList } from "./linkedlists.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list.toString());
+
 
